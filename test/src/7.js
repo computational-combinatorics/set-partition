3afb1591c6667a9dd132736b917366e67ab79315
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-12-24 18:45:55 +0100

    :mag: test: Complete test for larger input.

--- test/src/7.js
+++ test/src/7.js
@@ -1,12 +1,26 @@
 import test from 'ava';
 
+import totalOrder from 'total-order';
+import {RedBlackTree} from '@aureooms/js-red-black-tree';
+
 import {enumerate, _count, isPartition} from '../../src';
 
 test('Check partitions of [7]', (t) => {
 	const set = [1, 2, 3, 4, 5, 6, 7];
 	const partitions = [...enumerate(set)];
+
+	// Check that number of partition is correct
 	t.is(partitions.length, _count(1, set.length));
+
+	// Check that each item is a partition
 	for (const partition of partitions) {
 		t.true(isPartition(set, partition));
+	}
+
+	// Check that partitions are pairwise distinct
+	const seen = new RedBlackTree(totalOrder);
+	for (const partition of partitions) {
+		t.false(seen.has(partition));
+		seen.add(partition);
 	}
 });
