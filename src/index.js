1a75e4c5d1aeb9af26b14d4258f6c444697c8b00
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-05-21 17:13:55 +0200

    :boom: refactor!: Use flat export style.
    BREAKING CHANGE: default export is gone.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-import _count from './_count.js';
-import _enumerate from './_enumerate.js';
-import _sequence from './_sequence.js';
-import count from './count.js';
-import enumerate from './enumerate.js';
-import isPartition from './isPartition.js';
-import sequence from './sequence.js';
-
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-export default {
-	_count,
-	_enumerate,
-	_sequence,
-	count,
-	enumerate,
-	isPartition,
-	sequence,
-};
-
-export {_count, _enumerate, _sequence, count, enumerate, isPartition, sequence};
+export {default as _count} from './_count.js';
+export {default as _enumerate} from './_enumerate.js';
+export {default as _sequence} from './_sequence.js';
+export {default as count} from './count.js';
+export {default as enumerate} from './enumerate.js';
+export {default as isPartition} from './isPartition.js';
+export {default as sequence} from './sequence.js';
